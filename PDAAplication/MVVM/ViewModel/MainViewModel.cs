363beef2a09d0237d966498da6230be0909fafdf
TobMit <80719513+tobmit@users.noreply.github.com>
2023-10-28 22:56:59 +0200

    Project GUI basic design

---
+++ PDAAplication/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PDAAplication.Core;
+
+namespace PDAAplication.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public RelayCommand GenerateDataCommand { get; set; }
+        public RelayCommand FindBuildingsCommand { get; set; }
+        public RelayCommand FindObjectCommand { get; set; }
+        public RelayCommand AddBuildingCommand { get; set; }
+
+        public MainViewModel()
+        {
+            InicializeButtons();
+        }
+
+        private void InicializeButtons()
+        {
+            GenerateDataCommand = new RelayCommand(o => { GenerateData(); });
+            FindBuildingsCommand = new RelayCommand(o => { FindBuildings(); });
+            FindObjectCommand = new RelayCommand(o => { FindObject(); });
+            AddBuildingCommand = new RelayCommand(o => { AddBuilding(); });
+        }
+
+        private void GenerateData()
+        {
+            //todo add functionality
+        }
+
+        private void FindBuildings()
+        {
+            //todo add functionality
+        }
+
+        private void FindObject()
+        {
+            //todo add functionality
+        }
+
+        private void AddBuilding()
+        {
+            //todo add functionality
+        }
+    }
+}
