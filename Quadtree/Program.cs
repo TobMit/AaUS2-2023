41d5e96b672fe48ea1ff07c8bccc771a4c103a9a
TobMit <80719513+tobmit@users.noreply.github.com>
2023-10-31 09:31:27 +0100

    Generátor inštrukticí fix

--- Quadtree/Program.cs
+++ Quadtree/Program.cs
@@ -16,26 +16,14 @@ public class Program
     private static double PROBABILITY_DEPTH = 0.01;
         
         
-    private static int HODNOTA = 10000000; // !! Nemeniť inak nebudú spravné súradnice
     private static int lastestLovest = int.MaxValue;
     private static int seed = 0;
-    private static int maxSeed = Int32.MaxValue;
+    private static int maxSeed = int.MaxValue;
     public static void Main()
     {
         if (parallel)
         {
-            // Parallel.ForEach(Partitioner.Create(seed, maxSeed), (range) =>
-            // {
-                // for (int iSeed = range.Item1; iSeed < range.Item2; iSeed++)
-                // {
-                    // int result = TestInstance(iSeed);
-                    // if (result < 30)
-                    // {
-                        // Console.WriteLine("Najdeny SEED: " + iSeed);
-                        // return;
-                    // }
-                // }
-            // });
+            
             Parallel.For(seed, maxSeed, (iSeed) =>
             {
                 try
@@ -102,34 +90,22 @@ public class Program
         {
             Console.WriteLine(i);
         }*/
-
-        // long hodnota =  1 * 10000000;
-        // Console.WriteLine(hodnota);
-        // Console.WriteLine(int.MaxValue);
-        // Console.WriteLine(double.MaxValue);
-        // Console.WriteLine(double.MinValue);
-        // int pocetDeleni = 0;
-
-        // while (hodnota > 1)
-        // {
-        // hodnota /= 2;
-        // pocetDeleni++;
-        // }
-        // Console.WriteLine(pocetDeleni);
     }
 
     private static int TestInstance(int seed)
     {
         Random rnd = new Random(seed);
-            
-            
+
+        bool seedOk = true;    
+        
         double MIN_X = NextDouble(-180, 165, rnd);
         double MAX_X = NextDouble(MIN_X+5,180, rnd );
         double MIN_Y = NextDouble(-90, 75, rnd);
         double MAX_Y = NextDouble(MIN_Y+5,90, rnd );
-        int startDepth = rnd.Next(10, 22);
+        //int startDepth = rnd.Next(10, 22);
         
-        Console.WriteLine("SEED: " + seed + "\n Min x: " + MIN_X + "\n Max x: " + MAX_X + "\n Min y: " + MIN_Y + "\n Max y: " + MAX_Y + "\n Start depth: " + startDepth);
+        //Console.WriteLine("SEED: " + seed + "\n Min x: " + MIN_X + "\n Max x: " + MAX_X + "\n Min y: " + MIN_Y + "\n Max y: " + MAX_Y + "\n Start depth: " + startDepth);
+        Console.WriteLine("SEED: " + seed + "\n Min x: " + MIN_X + "\n Max x: " + MAX_X + "\n Min y: " + MIN_Y + "\n Max y: " + MAX_Y);
         
         
         List<int> toInsert = new(MAX_UNITS);
@@ -141,7 +117,7 @@ public class Program
         }
 
         QuadTree<int> quadtree = new QuadTree<int>(MIN_X, MIN_Y, 
-            Math.Abs(MIN_X - MAX_X), Math.Abs(MIN_Y - MAX_Y), startDepth);
+            Math.Abs(MIN_X - MAX_X), Math.Abs(MIN_Y - MAX_Y));
         for (int i = 0; i < MAX_TEST; i++)
         {
             double x = NextDouble(MIN_X, MAX_X-2, rnd);
@@ -152,12 +128,15 @@ public class Program
             var rndValue = rnd.NextDouble();
             if (rndValue < PROBABILITY_DEPTH)
             {
-                var newDepth = rnd.Next(10, 22);
+                var newDepth = rnd.Next(30, 200);
                 quadtree.SetQuadTreeDepth(newDepth);
                 if (quadtree.Count != quadtree.Recount())
                 {
+                    seedOk = false;
                     lastestLovest = i;
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Lastest lowest in set depth: " + lastestLovest + " in seed: " + seed);
+                    Console.ForegroundColor = ConsoleColor.White;
                     return lastestLovest;
                 }
             }
@@ -166,12 +145,15 @@ public class Program
                 
                 if (rnd.NextDouble() < 0.5)
                 {
-                    var tmpData = quadtree.FindInterval(MIN_X, MIN_Y, MAX_X, MAX_Y);
+                    var tmpData = quadtree.FindInterval(MIN_X, MIN_Y, MIN_X + Math.Abs(MIN_X - MAX_X), MIN_Y + Math.Abs(MIN_Y - MAX_Y));
                     if (tmpData.Count != toDelete.Count)
                     {
+                        seedOk = false;
                         lastestLovest = i;
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Lastest lowest in find interval: " + lastestLovest + " in seed: " + seed);
                         Console.WriteLine("     Find: " + tmpData.Count + " toDelete: " + toDelete.Count + " realCount " + quadtree.Recount());
+                        Console.ForegroundColor = ConsoleColor.White;
                         return lastestLovest;
                     }
                 }
@@ -180,9 +162,12 @@ public class Program
                     var tmpData = quadtree.FindOverlapingData(MIN_X, MIN_Y, MAX_X, MAX_Y);
                     if (tmpData.Count != toDelete.Count)
                     {
+                        seedOk = false;
                         lastestLovest = i;
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Lastest lowest in find overlaping: " + lastestLovest + " in seed: " + seed);
                         Console.WriteLine("     Find: " + tmpData.Count + " toDelete: " + toDelete.Count + " realCount " + quadtree.Recount());
+                        Console.ForegroundColor = ConsoleColor.White;
                         return lastestLovest;
                     }
                 }
@@ -204,17 +189,23 @@ public class Program
                         x2, y2);
                     if (!insertedValue.Contains(value))
                     {
+                        seedOk = false;
                         lastestLovest = i;
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Lastest lowest in insert: " + lastestLovest + " in seed: " + seed);
+                        Console.ForegroundColor = ConsoleColor.White;
                         return lastestLovest;
                     }
 
                     var realCount = quadtree.Recount();
                     if (realCount != toDelete.Count)
                     {
+                        seedOk = false;
                         lastestLovest = i;
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Lastest lowest in insert: " + lastestLovest + " in seed: " + seed);
                         Console.WriteLine("real count: " + realCount + " toDelete: " + toDelete.Count);
+                        Console.ForegroundColor = ConsoleColor.White;
                         return lastestLovest;
                     }
                 }
@@ -233,20 +224,26 @@ public class Program
                     var value = toDelete[index];
                     toDelete.RemoveAt(index);
                     toInsert.Add(value.Data);
-                    var deltedValue = quadtree.Delete((double)value.PointDownLeft.X / HODNOTA, (double)value.PointDownLeft.Y / HODNOTA,
-                        (double)value.PointUpRight.X / HODNOTA, (double)value.PointUpRight.Y / HODNOTA);
+                    var deltedValue = quadtree.Delete((double)value.PointDownLeft.X, (double)value.PointDownLeft.Y,
+                        (double)value.PointUpRight.X, (double)value.PointUpRight.Y);
                     if (deltedValue.Count == 0)
                     {
+                        seedOk = false;
                         lastestLovest = i;
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Lastest lowest in delete returned empty: " + lastestLovest + " in seed: " + seed);
                         Console.WriteLine("SEED: " + seed);
+                        Console.ForegroundColor = ConsoleColor.White;
                         return lastestLovest;
                     }
                     if (!deltedValue.Contains(value.Data))
                     {
+                        seedOk = false;
                         lastestLovest = i;
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Lastest lowest in delete: " + lastestLovest + " in seed: " + seed);
                         Console.WriteLine("SEED: " + seed);
+                        Console.ForegroundColor = ConsoleColor.White;
                         return lastestLovest;
                     }
 
@@ -269,9 +266,12 @@ public class Program
                     var realCount = quadtree.Recount();
                     if (realCount != toDelete.Count)
                     {
+                        seedOk = false;
                         lastestLovest = i;
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Lastest lowest in delete: " + lastestLovest + " in seed: " + seed);
                         Console.WriteLine("real count: " + realCount + " toDelete: " + toDelete.Count + " in seed: " + seed);
+                        Console.ForegroundColor = ConsoleColor.White;
                         return lastestLovest;
                     }
                     
@@ -284,11 +284,17 @@ public class Program
             }
         }
 
+        if (seedOk)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("SEED OK: " + seed);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
         return int.MaxValue;
     }
 
     private static double NextDouble(double min, double max, Random rnd)
     {
-        return rnd.NextDouble() * (max - min) + min;
+        return Math.Round(rnd.NextDouble() * (max - min) + min, 8);
     }
 }
