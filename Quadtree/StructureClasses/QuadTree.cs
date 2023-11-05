ddbd24a25cc3b65429098545beda11d98b0016fa
TobMit <80719513+tobmit@users.noreply.github.com>
2023-11-05 13:40:58 +0100

    Find fix

--- Quadtree/StructureClasses/QuadTree.cs
+++ Quadtree/StructureClasses/QuadTree.cs
@@ -237,7 +237,7 @@ public class QuadTree<TKey, TValue> where TKey : IComparable<TKey> where TValue 
             if (current.ModeOrDepth == 0)
             { 
                 // 1. hľadanie poduzla do ktorého sa zmesti hľadaná area
-                if (current.Node.ContainNode(areaToFind))
+                if (current.Node.OverlapNode(areaToFind))
                 {
                     // hľadanie prebieha tak že sa pozeráme ktorý uzol vie obsiahnuť vymazávanú areu a či aj potomkovia dokážu obsiahnuť hľadanú areu
                     if (current.Node.AnyInitSubNodeContainDataNode(areaToFind))
