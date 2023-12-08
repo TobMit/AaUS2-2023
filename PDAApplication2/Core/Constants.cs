3a40ac31d42a45cbc206fa30e59a2db229184bd3
TobMit <80719513+tobmit@users.noreply.github.com>
2023-12-08 11:46:05 +0100

    PDAApplication2 GUI - init

---
+++ PDAApplication2/Core/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDAApplication2.Core
+{
+    public enum ObjectType
+    {
+        Nehnutelnost = 0,
+        Parcela = 1
+    }
+
+    public static class Constants
+    {
+        public static double MAX_SIZE_OF_OBJCET_PERCENTAGE = 0.15;
+        public static int MAX_SIZE_TO_SHOW = 500;
+    }
+}
---
+++ PDAApplication2/Core/DataManager/FileManager/DataSaver.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using PDAApplication2.Interface;
+
+namespace PDAApplication2.Core.DataManager.FileManager
+{
+    class DataSaver<T> where T : ISavable
+    {
+        private StringBuilder builder;
+        public DataSaver()
+        {
+            builder = new();
+        }
+
+        /// <summary>
+        /// Umožňuje pridať 1 riadok záznamu ktorý sa uloží do súboru
+        /// </summary>
+        /// <param name="line"></param>
+        public void AddLine(string line)
+        {
+            builder.Append(line);
+        }
+
+        /// <summary>
+        /// Pripraví data na uloženie do súboru
+        /// </summary>
+        /// <param name="dataToSave"> Dáta ktoré chceme uložiť do súboru</param>
+        public void PrepareForSave(List<T> dataToSave)
+        {
+            foreach (T data in dataToSave)
+            {
+                builder.Append(data.ToSave());
+            }
+        }
+
+        /// <summary>
+        /// Uloží dáta do súboru asynchronne 
+        /// </summary>
+        public async void SaveData()
+        {
+            // modal window for save
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.FileName = "Data.csv";
+            saveFileDialog1.Filter = "Data (*.csv)|*.csv";
+            saveFileDialog1.Title = "Save to a file";
+            
+
+            if (saveFileDialog1.ShowDialog() == true)
+            {
+                using (StreamWriter outputFile = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    await outputFile.WriteAsync(builder.ToString());
+                }
+            }
+        }
+
+    }
+}
